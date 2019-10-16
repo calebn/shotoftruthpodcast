bcff52a802803ebfdbf1fdd05905ec9883257d0c
Caleb Nelson <caleb@sudo.science>
2019-10-16 14:54:43 -0700

    updated path to rss xml

--- scripts/site-gen.php
+++ scripts/site-gen.php
@@ -5,7 +5,7 @@ ini_set("xdebug.var_display_max_children", -1);
 ini_set("xdebug.var_display_max_data", -1);
 ini_set("xdebug.var_display_max_depth", -1);
 
-$xml_file = 'shotoftruthpodcastrss.xml';
+$xml_file = '../shotoftruthpodcastrss.xml';
 
 $res = file_get_contents($xml_file);
 
