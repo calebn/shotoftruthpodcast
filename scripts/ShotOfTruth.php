f8e5226dbf703468374e9cdbbc3151d7d9967ebc
Caleb Nelson <caleb@sudo.science>
2019-10-18 16:58:48 -0700

    added favicons

--- scripts/ShotOfTruth.php
+++ scripts/ShotOfTruth.php
@@ -41,6 +41,11 @@ namespace shotoftruth {
 				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 				<link rel="stylesheet" href="/styles.css">
 
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+				<link rel="manifest" href="/site.webmanifest">
+
 				<title><?=$title?></title>
 
 				<!-- Global site tag (gtag.js) - Google Analytics -->
